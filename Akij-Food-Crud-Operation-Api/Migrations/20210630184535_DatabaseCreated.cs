303655c04bd29f882427541c92929e137aee6d62
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-01 01:15:08 +0600

    Implement Entity classes and add seeding data

---
+++ Akij-Food-Crud-Operation-Api/Migrations/20210630184535_DatabaseCreated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Akij_Food_Crud_Operation_Api.Migrations
+{
+    public partial class DatabaseCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ColdDrinks",
+                columns: table => new
+                {
+                    ColdDrinksId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ColdDrinksName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ColdDrinks", x => x.ColdDrinksId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ColdDrinks");
+        }
+    }
+}
