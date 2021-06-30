67c26d3a6010883ff904eeba6ab6a8664d76a7e6
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-01 01:50:32 +0600

    Added Repository Pattern

---
+++ Akij-Food-Crud-Operation-Api/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Akij_Food_Crud_Operation_Api.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akij_Food_Crud_Operation_Api.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<ColdDrink> ColdDrinks { get; }
+        Task Save();
+    }
+}
