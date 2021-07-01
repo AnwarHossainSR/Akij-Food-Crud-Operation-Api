f40badd573e1d11b1d1cf1ebed4b72fed4c7c18c
AnwarHossainSR <mahedianwar@gmail.com>
2021-07-01 10:14:18 +0600

    Added Auto Mapper And DTOs

---
+++ Akij-Food-Crud-Operation-Api/Configuration/MapperInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Akij_Food_Crud_Operation_Api.data;
+using Akij_Food_Crud_Operation_Api.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akij_Food_Crud_Operation_Api.Configuration
+{
+    public class MapperInitializer : Profile
+    {
+        public MapperInitializer()
+        {
+            CreateMap<ColdDrink, ColdDrinkDTO>().ReverseMap();
+            CreateMap<ColdDrink, CreateColdDrinkDTO>().ReverseMap();
+        }
+    }
+}
